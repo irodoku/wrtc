a000b6437c83ba26697ee3f2deee12c9ee2247bd
irodoku <46772774+irodoku@users.noreply.github.com>
2022-08-25 20:03:03 -0300

    nombre botones

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ let remoteStream;
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
     remoteStream = new MediaStream()
-    document.getElementById('user-1').srcObject = localStream
-    document.getElementById('user-2').srcObject = remoteStream
-
+    document.getElementById('id-1').srcObject = localStream
+    document.getElementById('id-2').srcObject = remoteStream
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
     });
@@ -29,49 +28,37 @@ let init = async () => {
     };
 }
 
-let createOffer = async () => {
-
-
+let crearOferta = async () => {
     peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new offer ICE candidate is created
         if(event.candidate){
-            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
+            document.getElementById('oferta-sdp').value = JSON.stringify(peerConnection.localDescription)
         }
     };
-
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
+    const oferta = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(oferta);
 }
 
-let createAnswer = async () => {
-
-    let offer = JSON.parse(document.getElementById('offer-sdp').value)
-
+let crearRespuesta = async () => {
+    let oferta = JSON.parse(document.getElementById('oferta-sdp').value)
     peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new answer ICE candidate is created
         if(event.candidate){
-            console.log('Adding answer candidate...:', event.candidate)
-            document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
+            document.getElementById('respuesta-sdp').value = JSON.stringify(peerConnection.localDescription)
         }
     };
-
-    await peerConnection.setRemoteDescription(offer);
-
-    let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer); 
+    await peerConnection.setRemoteDescription(oferta);
+    let respuesta = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(respuesta);
 }
 
 let addAnswer = async () => {
-    console.log('Add answer triggerd')
-    let answer = JSON.parse(document.getElementById('answer-sdp').value)
-    console.log('answer:', answer)
+    let respuesta = JSON.parse(document.getElementById('respuesta-sdp').value)
     if (!peerConnection.currentRemoteDescription){
-        peerConnection.setRemoteDescription(answer);
+        peerConnection.setRemoteDescription(respuesta);
     }
 }
 
 init()
 
-document.getElementById('create-offer').addEventListener('click', createOffer)
-document.getElementById('create-answer').addEventListener('click', createAnswer)
-document.getElementById('add-answer').addEventListener('click', addAnswer)
+document.getElementById('create-oferta').addEventListener('click', crearOferta)
+document.getElementById('create-respuesta').addEventListener('click', crearRespuesta)
+document.getElementById('add-respuesta').addEventListener('click', addAnswer)
